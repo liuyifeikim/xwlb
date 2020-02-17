57565b2ec4c5cef3bc3a6d975b9d26b53ebf09b9
KIM LIU <liuyifeikim@163.com>
2020-02-17 23:32:04 +0800

    Add files via upload

--- xwlb_scraping_fun.R
+++ xwlb_scraping_fun.R
@@ -30,10 +30,14 @@ all_scraping_fun_new <- function(){
   page <- read_html(out_url)                                        #读取列表页内容
   last_page <- page %>% html_nodes("a:nth-child(13)") %>% html_attr("href") %>% str_extract("\\d+") %>% as.integer()
   #调用多线程:snow的sock集群
-  cl <- makeCluster(cl)
+  cl <- makeCluster(detectCores())
   registerDoParallel(cl)
   #循环所有列表页，提取内页url列表
-  in_url_list <- unlist(foreach(i = seq(1, last_page)) %dopar% in_url_scraping_fun(i))
+  in_url_vec <- foreach(i = seq(1, last_page), .combine = c) %dopar% in_url_scraping_fun(i)
   #从内页循环提取新闻内容，并按行堆叠
-  foreach(j = seq(1, length(in_url_list)), .combine = rbind) %dopar% news_scraping_fun(in_url_list[j])
+  result <- foreach(j = seq(1, length(in_url_list)), .combine = rbind) %dopar% news_scraping_fun(in_url_vec[j])
+  #停止集群，一定要在return前，否则无法停止集群
+  stopCluster(cl)  
+  #返回结果，一定要有，否则结果为空
+  return(result)  
 }
